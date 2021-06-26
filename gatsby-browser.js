495633bf20ad1ef238fd36cbe318744e70ded567
kashiwade <music5d7_1415@outlook.com>
2021-06-26 10:02:37 +0900

    fixed slidshow

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,4 @@ import "@fontsource/kanit/400.css";
 import "@fontsource/noto-sans-jp/300.css";
 import "@fontsource/noto-sans-jp/100.css";
 import "@fortawesome/free-brands-svg-icons";
+import "react-slideshow-image/dist/styles.css";
