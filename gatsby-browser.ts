2e280c9e4a9fc83e9f7656f486502dd90419b37d
kashiwade <music5d7_1415@outlook.com>
2022-12-09 20:32:15 +0900

    feat: add 明朝体 font

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -4,4 +4,7 @@ import "@fontsource/kanit/300.css";
 import "@fontsource/kanit/400.css";
 import "@fontsource/noto-sans-jp/300.css";
 import "@fontsource/noto-sans-jp/100.css";
+import "@fontsource/zen-old-mincho/400.css";
+import "@fontsource/zen-old-mincho/500.css";
+import "@fontsource/zen-old-mincho/600.css";
 import "react-slideshow-image/dist/styles.css";
